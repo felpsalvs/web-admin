aa192bdf4f7ba048525830d0035e20c863662cf8
Felipe Alves <profelipealves@outlook.com>
2024-08-06 11:00:30 -0300

    chore: added useMemo for context theme

--- src/theme.js
+++ src/theme.js
@@ -189,3 +189,23 @@ return {
     },
   },
 };
+
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {},
+});
+
+export const useMode = () => {
+  const [mode, setMode] = useState("dark");
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () =>
+        setMode((prev) => (prev === "light" ? "dark" : "light")),
+    }),
+    []
+  );
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return [theme, colorMode];
+};
