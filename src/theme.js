e5942b923a0e4a14a83cf9d93b2974aa24e5fe40
Felipe Alves <profelipealves@outlook.com>
2024-08-06 13:22:17 -0300

    chore: updated context and folder

--- src/theme.js
+++ src/theme.js
@@ -121,73 +121,73 @@ export const tokens = (mode) => ({
 
 export const themeSettings = (mode) => {
   const colors = tokens(mode);
-};
 
-return {
-  palette: {
-    mode: mode,
-    ...(mode === "dark"
-      ? {
-          primary: {
-            main: colors.primary[500],
-          },
-          secondary: {
-            main: colors.secondary[500],
-          },
-          neutral: {
-            dark: colors.grey[700],
-            main: colors.grey[500],
-            light: colors.grey[100],
-          },
-          background: {
-            default: colors.black[500],
-          },
-        }
-      : {
-          primary: {
-            main: colors.primary[100],
-          },
-          secondary: {
-            main: colors.secondary[500],
-          },
-          neutral: {
-            dark: colors.grey[700],
-            main: colors.grey[500],
-            light: colors.grey[100],
-          },
-          background: {
-            default: "#fcfcfc",
-          },
-        }),
-  },
-  typography: {
-    fontFamily: ["Roboto", "sans-serif"].join(","),
-    fontSize: 12,
-    h1: {
-      fontFamily: ["Roboto", "sans-serif"].join(","),
-      fontSize: 40,
+  return {
+    palette: {
+      mode: mode,
+      ...(mode === "dark"
+        ? {
+            primary: {
+              main: colors.primary[500],
+            },
+            secondary: {
+              main: colors.secondary[500],
+            },
+            neutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: colors.black[500],
+            },
+          }
+        : {
+            primary: {
+              main: colors.primary[100],
+            },
+            secondary: {
+              main: colors.secondary[500],
+            },
+            neutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: "#fcfcfc",
+            },
+          }),
     },
-    h2: {
+    typography: {
       fontFamily: ["Roboto", "sans-serif"].join(","),
-      fontSize: 32,
+      fontSize: 12,
+      h1: {
+        fontFamily: ["Roboto", "sans-serif"].join(","),
+        fontSize: 40,
+      },
+      h2: {
+        fontFamily: ["Roboto", "sans-serif"].join(","),
+        fontSize: 32,
+      },
+      h3: {
+        fontFamily: ["Roboto", "sans-serif"].join(","),
+        fontSize: 24,
+      },
+      h4: {
+        fontFamily: ["Roboto", "sans-serif"].join(","),
+        fontSize: 20,
+      },
+      h5: {
+        fontFamily: ["Roboto", "sans-serif"].join(","),
+        fontSize: 16,
+      },
+      h6: {
+        fontFamily: ["Roboto", "sans-serif"].join(","),
+        fontSize: 14,
+      },
     },
-    h3: {
-      fontFamily: ["Roboto", "sans-serif"].join(","),
-      fontSize: 24,
-    },
-    h4: {
-      fontFamily: ["Roboto", "sans-serif"].join(","),
-      fontSize: 20,
-    },
-    h5: {
-      fontFamily: ["Roboto", "sans-serif"].join(","),
-      fontSize: 16,
-    },
-    h6: {
-      fontFamily: ["Roboto", "sans-serif"].join(","),
-      fontSize: 14,
-    },
-  },
+  };
 };
 
 export const ColorModeContext = createContext({
