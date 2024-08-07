a07d13965ac12dce0915d697c873dba6ef2ec562
Felipe Alves <profelipealves@outlook.com>
2024-08-07 18:45:50 -0300

    chore: create scenes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,18 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 
 import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar"
 import Home from "./scenes/home";
+import Pacttos from "./scenes/pacttos"
+import Review from "./scenes/review"
+import Shared from "./scenes/shared"
+import Info from "./scenes/info"
+import Packages from "./scenes/packages"
+import Profile from "./scenes/profile"
+import Library from "./scenes/library"
+import Subscription from "./scenes/subscription"
+
+
 
 function App() {
   const [theme, colorMode] = useMode();
