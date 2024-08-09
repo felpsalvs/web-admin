30100c1f7722474c3f3ebb1d0474f25cf9b40760
Felipe Alves <profelipealves@outlook.com>
2024-08-08 23:37:07 -0300

    chore:
    prepare router setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar"
 import Home from "./scenes/home";
-import Pacttos from "./scenes/pacttos"
-import Review from "./scenes/review"
-import Shared from "./scenes/shared"
-import Info from "./scenes/info"
-import Packages from "./scenes/packages"
-import Profile from "./scenes/profile"
-import Library from "./scenes/library"
-import Subscription from "./scenes/subscription"
+// import Pacttos from "./scenes/pacttos"
+// import Review from "./scenes/review"
+// import Shared from "./scenes/shared"
+// import Info from "./scenes/info"
+// import Packages from "./scenes/packages"
+// import Profile from "./scenes/profile"
+// import Library from "./scenes/library"
+// import Subscription from "./scenes/subscription"
 
 
 
@@ -24,10 +24,19 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/pacttos" element={<Pacttos />} />
+              <Route path="/review" element={<Review />} />
+              <Route path="/shared" element={<Shared />} />
+              <Route path="/info" element={<Info />} />
+              <Route path="/packages" element={<Packages />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/library" element={<Library />} />
+              <Route path="/subscription" element={<Subscription />} />
             </Routes>
           </main>
         </div>
