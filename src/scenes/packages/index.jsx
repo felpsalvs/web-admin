310cc50edb3eb75db479013b8874ebf62615b54d
Felipe Alves <profelipealves@outlook.com>
2024-08-15 23:14:01 -0300

    chore: update typography style and add buttons

--- src/scenes/packages/index.jsx
+++ src/scenes/packages/index.jsx
@@ -1,141 +1,174 @@
-import { useState } from 'react';
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, FormControlLabel, Checkbox, MenuItem } from '@mui/material';
+import { useState } from "react"
+import {
+	Box,
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogTitle,
+	TextField,
+	FormControlLabel,
+	Checkbox,
+	MenuItem,
+} from "@mui/material"
+import PlusIcon from "@mui/icons-material/Add"
 
 const Packages = () => {
-  const [open, setOpen] = useState(false);
+	const [open, setOpen] = useState(false)
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+	const handleClickOpen = () => {
+		setOpen(true)
+	}
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+	const handleClose = () => {
+		setOpen(false)
+	}
 
-  return (
-    <Box p="20px">
-      <Box display="flex" justifyContent="space-between" mb="20px">
-        <Button variant="contained" color="secondary" onClick={handleClickOpen}>
-          + ADD PACKAGE
-        </Button>
-        <Button variant="contained" disabled>
-          SAVE CHANGES
-        </Button>
-        <Button variant="contained" color="secondary">
-          PAYMENT DETAILS
-        </Button>
-      </Box>
+	return (
+		<Box p="20px">
+			<Box display="flex" justifyContent="space-between" mb="20px">
+				<Button
+					onClick={handleClickOpen}
+					variant="contained"
+					color="secondary"
+					sx={{
+						ml: 2,
+						backgroundColor: "secondary",
+						color: "#000",
+						fontWeight: "600",
+					}}
+					startIcon={<PlusIcon />}
+				>
+					ADD PACKAGE
+				</Button>
 
-      <Dialog open={open} onClose={handleClose} maxWidth="lg" fullWidth>
-        <DialogTitle>Describe your package</DialogTitle>
-        <DialogContent>
-          <Box display="grid" gridTemplateColumns="repeat(3, 1fr)" gap="20px">
-            <Box>
-              <TextField
-                fullWidth
-                label="Package name"
-                variant="filled"
-                defaultValue="Change the name of package"
-              />
-              <TextField
-                fullWidth
-                label="Package description"
-                variant="filled"
-                multiline
-                rows={4}
-                placeholder="Add description to this package"
-                sx={{ mt: 2 }}
-              />
-              <TextField
-                fullWidth
-                label="Package details (user will view only)"
-                variant="filled"
-                multiline
-                rows={4}
-                placeholder="Add details"
-                sx={{ mt: 2 }}
-              />
-              <TextField
-                fullWidth
-                label="Package instructions (customers will view too)"
-                variant="filled"
-                multiline
-                rows={4}
-                placeholder="Add instructions"
-                sx={{ mt: 2 }}
-              />
-            </Box>
+				<Box sx={{ display: "flex", justifyContent: "flex-end", gap: 1 }}>
+					<Button
+						variant="contained"
+						disabled
+						sx={{ display: "flex", justifyContent: "flex-end" }}
+					>
+						SAVE CHANGES
+					</Button>
+					<Button
+						variant="contained"
+						color="secondary"
+						sx={{
+							ml: 2,
+							backgroundColor: "secondary",
+							color: "#000",
+							fontWeight: "600",
+						}}
+					>
+						PAYMENT DETAILS
+					</Button>
+				</Box>
+			</Box>
 
-            <Box>
-              <Box mb={2}>
-                <FormControlLabel
-                  control={<Checkbox />}
-                  label="Single file review (e.g. video, image, or PDF)"
-                />
-                <FormControlLabel
-                  control={<Checkbox />}
-                  label="Single link review (e.g. LinkedIn URL, portfolio)"
-                />
-                <FormControlLabel
-                  control={<Checkbox />}
-                  label="Single review of multiple files (e.g. 2 videos and 1 image, reviewed together)"
-                />
-              </Box>
-              <Box mb={2}>
-                <Box mb={1}>Review file type</Box>
-                <FormControlLabel
-                  control={<Checkbox />}
-                  label="Video"
-                />
-                <FormControlLabel
-                  control={<Checkbox />}
-                  label="Image"
-                />
-                <FormControlLabel
-                  control={<Checkbox />}
-                  label="File (PDF, txt, etc)"
-                />
-              </Box>
-            </Box>
+			<Dialog open={open} onClose={handleClose} maxWidth="lg" fullWidth>
+				<DialogTitle>Describe your package</DialogTitle>
+				<DialogContent>
+					<Box display="grid" gridTemplateColumns="repeat(3, 1fr)" gap="20px">
+						<Box>
+							<TextField
+								fullWidth
+								label="Package name"
+								variant="filled"
+								defaultValue="Change the name of package"
+							/>
+							<TextField
+								fullWidth
+								label="Package description"
+								variant="filled"
+								multiline
+								rows={4}
+								placeholder="Add description to this package"
+								sx={{ mt: 2 }}
+							/>
+							<TextField
+								fullWidth
+								label="Package details (user will view only)"
+								variant="filled"
+								multiline
+								rows={4}
+								placeholder="Add details"
+								sx={{ mt: 2 }}
+							/>
+							<TextField
+								fullWidth
+								label="Package instructions (customers will view too)"
+								variant="filled"
+								multiline
+								rows={4}
+								placeholder="Add instructions"
+								sx={{ mt: 2 }}
+							/>
+						</Box>
 
-            <Box>
-              <TextField
-                fullWidth
-                label="Currency"
-                variant="filled"
-                              defaultValue="USD"
-                sx={{ mb: 2 }}
-              >
-                <MenuItem value="USD">USD</MenuItem>
-                <MenuItem value="EUR">EUR</MenuItem>
-                <MenuItem value="BRL">BRL</MenuItem>
-              </TextField>
-              <TextField
-                fullWidth
-                label="Package cost"
-                variant="filled"
-                type="number"
-                defaultValue="25"
-                sx={{ mb: 2 }}
-              />
-              <TextField
-                fullWidth
-                label="Number of reviews (credits) in this package"
-                variant="filled"
-                type="number"
-                defaultValue="1"
-              />
-            </Box>
-          </Box>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="secondary" variant="contained">
-            Publish Package
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </Box>
-  );
-};
+						<Box>
+							<Box mb={2}>
+								<FormControlLabel
+									control={<Checkbox />}
+									label="Single file review (e.g. video, image, or PDF)"
+								/>
+								<FormControlLabel
+									control={<Checkbox />}
+									label="Single link review (e.g. LinkedIn URL, portfolio)"
+								/>
+								<FormControlLabel
+									control={<Checkbox />}
+									label="Single review of multiple files (e.g. 2 videos and 1 image, reviewed together)"
+								/>
+							</Box>
+							<Box mb={2}>
+								<Box mb={1}>Review file type</Box>
+								<FormControlLabel control={<Checkbox />} label="Video" />
+								<FormControlLabel control={<Checkbox />} label="Image" />
+								<FormControlLabel
+									control={<Checkbox />}
+									label="File (PDF, txt, etc)"
+								/>
+							</Box>
+						</Box>
 
-export default Packages;
+						<Box>
+							<TextField
+								fullWidth
+								label="Currency"
+								variant="filled"
+								defaultValue="USD"
+								sx={{ mb: 2 }}
+							>
+								<MenuItem value="USD">USD</MenuItem>
+								<MenuItem value="EUR">EUR</MenuItem>
+								<MenuItem value="BRL">BRL</MenuItem>
+							</TextField>
+							<TextField
+								fullWidth
+								label="Package cost"
+								variant="filled"
+								type="number"
+								defaultValue="25"
+								sx={{ mb: 2 }}
+							/>
+							<TextField
+								fullWidth
+								label="Number of reviews (credits) in this package"
+								variant="filled"
+								type="number"
+								defaultValue="1"
+							/>
+						</Box>
+					</Box>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose} color="secondary" variant="contained">
+						Publish Package
+					</Button>
+				</DialogActions>
+			</Dialog>
+		</Box>
+	)
+}
+
+export default Packages
